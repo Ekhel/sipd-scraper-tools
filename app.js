4b3c6224c353b0cb7a359e20217653619060ee25
Ekhel <blackmanskill@gmail.com>
2021-10-29 18:07:56 +0900

    UI for easlyuse

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
-
+const hbs = require('hbs');
+const path = require('path');
 const app = express();
+
+const viewPath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
+
+app.get('', (req, res) => {
+    res.render('home', {
+        title: 'SIPD Scraper Tools',
+        name: 'Ekhel'
+    });
+});
 
 module.exports = app;
