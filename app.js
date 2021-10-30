01a98733c3c1e528b2be40277258512ced1e850c
ekhel <blackmanskill@gmail.com>
2021-10-31 02:13:24 +0900

    SaveConfigJSON

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
 const renjaRoutes = require('./routes/renjaRoute');
@@ -13,6 +15,9 @@ app.set('view engine', 'hbs');
 app.set('views', viewPath);
 hbs.registerPartials(partialsPath);
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('', (req, res) => {
     res.render('home', {
         title: 'SIPD Scraper Tools',
@@ -20,7 +25,36 @@ app.get('', (req, res) => {
     });
 });
 
+app.get('/config', (req, res) => {
+    res.render('config', {
+        title: 'Config Auth'
+    });
+});
+
+app.post('/saveconfig', (req, res) => {
+    const body = req.body;
+    var set = { 
+        tahun_anggaran: body.tahun_anggaran, 
+        id_daerah:body.id_daerah, 
+        sipd_url: body.sipd_url, 
+        user_name: body.user_name, 
+        user_pass: body.user_pass 
+    }
+
+    const finished = (err) => {
+        if (err) {
+            console.log(err)
+            return;
+        }
+    }
+    
+    const dataSetJson = JSON.stringify(set);
+    fs.writeFile('./utils/dataConfig.json',dataSetJson,finished);
+
+    return res.json(req.body);
+})
+
 app.use('/data/listing', renjaRoutes);
-app.use('/data/config', configRoutes);
+
 
 module.exports = app;
--- routes/configRoute.js
+++ routes/configRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const renjaController = require('../controllers/configController');
+const configController = require('../controllers/configController');
 
+router.route('/saveconfig', configController.saveconfig).post();
 router.get('/setconfigauth', (req, res) => {
     res.render('config', {
         title: 'Seeting Auth SIPD'
